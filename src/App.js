e8a77b9a815ca87fafb35bbf03349f9e56605069
sanjeev <sanjeev.kumar@shipease.in>
2024-05-05 13:22:57 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p className="text-danger">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod iste facilis fugiat officia sapiente minima magnam in, dicta cupiditate? Magni neque debitis vero obcaecati voluptatibus natus necessitatibus saepe explicabo nostrum?</p>
       </header>
     </div>
   );
